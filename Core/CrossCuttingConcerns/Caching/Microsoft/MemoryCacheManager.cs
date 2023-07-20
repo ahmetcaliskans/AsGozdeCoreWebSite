128c479964ed30d0709e07a2c23557cf656dad9d
ahmetcaliskans <angelmetodiev@gmail.com>
2023-07-20 11:36:07 +0300

    caching güncelleme yapildi

--- Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
+++ Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
@@ -39,20 +39,20 @@ namespace Core.CrossCuttingConcerns.Caching.Microsoft
 
         public void RemoveByPattern(string pattern)
         {
-            var splitpattern = pattern.Split(",");
-            foreach (var splitedpatters in splitpattern)
+            var cacheEntriesCollectionDefinition = typeof(MemoryCache).GetProperty("EntriesCollection", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var cacheEntriesCollection = cacheEntriesCollectionDefinition.GetValue(_memorycache) as dynamic;
+            List<ICacheEntry> cacheCollectionValues = new List<ICacheEntry>();
+
+            foreach (var cacheItem in cacheEntriesCollection)
             {
-                var cacheEntriesCollectionDefinition = typeof(MemoryCache).GetProperty("EntriesCollection", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-                var cacheEntriesCollection = cacheEntriesCollectionDefinition.GetValue(_memorycache) as dynamic;
-                List<ICacheEntry> cacheCollectionValues = new List<ICacheEntry>();
+                ICacheEntry cacheItemValue = cacheItem.GetType().GetProperty("Value").GetValue(cacheItem, null);
+                cacheCollectionValues.Add(cacheItemValue);
+            }
 
-                foreach (var cacheItem in cacheEntriesCollection)
-                {
-                    ICacheEntry cacheItemValue = cacheItem.GetType().GetProperty("Value").GetValue(cacheItem, null);
-                    cacheCollectionValues.Add(cacheItemValue);
-                }
-
-                var regex = new Regex(splitedpatters, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var splitpattern = pattern.Split(",");
+            foreach (var splitedpattern in splitpattern)
+            {
+                var regex = new Regex(splitedpattern, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
                 var keysToRemove = cacheCollectionValues.Where(d => regex.IsMatch(d.Key.ToString())).Select(d => d.Key).ToList();
 
                 foreach (var key in keysToRemove)
@@ -60,6 +60,8 @@ namespace Core.CrossCuttingConcerns.Caching.Microsoft
                     _memorycache.Remove(key);
                 }
             }
+            
+            
             
         }
     }
